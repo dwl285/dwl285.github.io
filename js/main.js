de5987c233dc03cd6f11501d437fe6b8c31fb343
Daniel Lee <dwl285@Daniels-MacBook-Pro.local>
2017-08-22 21:08:36 +0100

    add hover effect to project cards, add description

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@
 var main = function() {
   $('.projectbox').hover(function() {
   	$(this).addClass('projecthover');
+  	console.log("test");
+  	$(this).find('div.description').fadeIn(20);
   }, function() {
   	$(this).removeClass('projecthover');
+  	$(this).find('div.description').fadeOut(20);
   });
 };
 
