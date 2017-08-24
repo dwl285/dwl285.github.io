2914032335b24fb146efd5125e34c62f0518df17
Daniel Lee <dwl285@Daniels-MacBook-Pro.local>
2017-08-24 09:44:23 +0100

    add project type selector buttons

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 
 var main = function() {
+  // if hovering on prject box, show description and add shadow
   $('.projectbox').hover(function() {
   	$(this).addClass('projecthover');
   	console.log("test");
@@ -8,6 +9,28 @@ var main = function() {
   }, function() {
   	$(this).removeClass('projecthover');
   	$(this).find('div.description').fadeOut(20);
+  });
+  
+  $('.projectselector li').click(function() {
+    var category = $(this).attr('class');
+    if(category==="selector-js") {
+      $('.projectbox').removeClass('selected');
+      $('.js').addClass('selected');
+    }
+    else if(category==="selector-html") {
+      $('.projectbox').removeClass('selected');
+      $('.html').addClass('selected');
+    }
+    else if(category==="selector-fun") {
+      $('.projectbox').removeClass('selected');
+      $('.fun').addClass('selected');
+    }
+    else if(category==="selector-all") {
+      $('.projectbox').addClass('selected')
+    }
+
+    $('.projectselector li').removeClass('active');
+    $(this).addClass('active');
   });
 };
 
